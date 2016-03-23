b19af2b693a897e76afad2d9426885d9f9dd6517
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-03-22 17:01:56 -0700

    Update dependencies.
    Resolve module lile node.

--- playground/clientEntry.tsx
+++ playground/clientEntry.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Playground from 'component-playground';
-import SeedComponent from '../src/index';
+import SeedComponent from '../src';
 
-const SeedComponentExample = require('raw!../src/SeedComponent.example');
+const SeedComponentExample: string = require('raw!../src/SeedComponent.example');
 
 const Index = () => (
   <div className="component-documentation">
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-import SeedComponent from './SeedComponent';
-
-export default SeedComponent;
+export { default } from './SeedComponent';
